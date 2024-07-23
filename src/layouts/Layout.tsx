4886dc778f8dd9c6955a521888e9be5e6ca49878
MicroCats37 <microcat_73@outlook.com>
2024-07-23 13:53:04 -0500

    void

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,16 +3,16 @@ import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
     <>
-        <header className='bg-slate-800'>
-            <div className='mx-auto max-w-6xl py-10'>
-                <h1 className='text-4xl font-extrabold text-white'>
-                    Administrador de Productos
+        <header className='bg-green-700 rounded-lg my-8 w-5/6 md:3/4 lg:w-2/4 mx-auto '>
+            <div className='mx-auto max-w-6xl py-10 text-center'>
+                <h1 className='text-4xl font-extrabold text-purple-900'>
+                Administrador de productos
                 </h1>
             </div>
         </header>
     
-        <main className='mt-10 mx-auto max-w-6xl p-10 bg-white shadow'>
-            <Outlet />
+        <main className='mt-10 mx-auto w-5/6 md:3/4 lg:w-2/4 p-10 bg-slate-300 shadow-md rounded-lg shadow-lime-800 scroll-mx-0 overflow-hidden'>
+            <Outlet/>
         </main>
     </>
   )
